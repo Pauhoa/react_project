da79fcf4896fd1bfa03f32b79d0e5748ee5498b2
Pauhoa <hoarau.paul3@gmail.com>
2022-03-18 20:39:00 +0400

    done

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 //? import './index.css';
 //? import App from './App';
 //?import reportWebVitals from './reportWebVitals';
 
-const Composant2 = (props) => {
-  return <h1>{props.name}</h1>
-}
-
 class Composant1 extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      compteur: 0,
+    };
+  }
+
+  increment = (event) => {
+    this.setState({
+      compteur: this.state.compteur + 1
+    })
   }
 
   render() {
     return (
       <div>
-        <Composant2 name="c2-1"></Composant2>
-        <Composant2 name="c2-2"/>
+        <span>{this.state.compteur}</span>
+        <br />
+        <button onClick={this.increment}>+1</button>
       </div>
-    )
+    );
   }
 }
 
-
-ReactDOM.render( <Composant1 name="world !!!" /> , document.getElementById('root'));
-
-
+ReactDOM.render(
+  <Composant1 name="world !!!" />,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
