275e82cff864b469d76e04eb44a4b4f42f4dd28a
Pauhoa <hoarau.paul3@gmail.com>
2022-03-19 15:09:43 +0400

    done

--- src/index.js
+++ src/index.js
@@ -20,14 +20,17 @@ class App extends Component {
     this.state = {
       people: [
         {
+          id: 1,
           name: 'Pierre',
           age: 12
         },
         {
+          id: 2,
           name: 'Paul',
           age: 13
         },
         {
+          id: 3,
           name: 'Jacques',
           age: 14
         }
@@ -36,7 +39,7 @@ class App extends Component {
   }
 
   deletePerson = (index) => {
-    const people = [... this.state.people];
+    const people = [...this.state.people];
     people.splice(index, 1);
     this.setState({
       people
@@ -48,7 +51,7 @@ class App extends Component {
       <div className="container d-flex flex-column justify-content-center align-items-center">
         <h1 className="mb-5">People</h1>
         <ul className="list-group w-100">
-          { this.state.people.map ( (p,index) => <Person person={p} deletePerson={ (index) => this.deletePerson(index)} />)}
+          { this.state.people.map ( (p,index) => <Person key={ p.id} person={p} deletePerson={ (index) => this.deletePerson(index)} />)}
         </ul>
       </div>
     )
