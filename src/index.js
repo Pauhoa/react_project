81ebb96a4240cf9731f208506ee1179a9ac97638
Pauhoa <hoarau.paul3@gmail.com>
2022-03-21 13:26:35 +0400

    fragments

--- src/index.js
+++ src/index.js
@@ -1,36 +1,25 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import './index.css';
 
-const Composant1 = (props) => {
-  return <h1> Composant 1</h1>;
+const Header = () => <div>Header</div>
+const Body = () => <div>Body</div>
+
+const Layout = () => {
+  return (
+    <>
+      <Header/>
+      <Body/>
+    </>
+  )
 }
-const Composant2 = () => <h1> Composant 2</h1>;
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <div className="p-2 w-100 d-flex border flex-row">
-            <Link to="/1" className="m-2">
-              Composant 1
-            </Link>
-            <Link to="/2" className="m-2">
-              Composant 2
-            </Link>
-          </div>
-          <div
-            style={{ minHeight: "100vh" }}
-            className="d-flex text-center flex-column justify-content-center"
-          >
-            <Routes>
-              <Route path="/" element={<Composant1/>}></Route>
-              <Route path="/2" element={<Composant2/>}></Route>
-            </Routes>
-          </div>
-        </div>
-      </Router>
+      <div className="container-fluide d-flex flex-column">
+        <Layout/>
+      </div>
     );
   }
 }
