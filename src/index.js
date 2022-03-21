b3ab1831eef76da71f7791793997dce907bafb94
Pauhoa <hoarau.paul3@gmail.com>
2022-03-21 15:13:42 +0400

    hoc done

--- src/index.js
+++ src/index.js
@@ -1,75 +1,61 @@
+import { toHaveAccessibleDescription } from "@testing-library/jest-dom/dist/matchers";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-const AuthContext = React.createContext({ isLoggedin: false });
-const ThemeContext = React.createContext("light");
-
-const Header = () => {
-  return (
-    <AuthContext.Consumer>
-      {(authContext) => (
-        <ThemeContext.Consumer>
-          {(themeContext) => (
-            <div className="navbar bg-light">
-              <div>Mon Logo  { themeContext }</div>
-              {authContext.auth.isLoggedin ? (
-                <div onClick={authContext.logout}>Deconnexion</div>
-              ) : (
-                <div onClick={authContext.login}>Connexion</div>
-              )}
-            </div>
-          )}
-        </ThemeContext.Consumer>
-      )}
-    </AuthContext.Consumer>
-  );
-};
-
-class Layout extends Component {
-  render() {
-    return <Header />;
-  }
-}
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      auth: {
-        isLoggedin: true,
-      },
-      login: this.login,
-      logout: this.logout,
-    };
-  }
-
-  login = () => {
-    this.setState({
-      auth: {
-        isLoggedin: true,
-      },
-    });
-  };
-
-  logout = () => {
-    this.setState({
-      auth: {
-        isLoggedin: false,
-      },
-    });
-  };
-
+class Composant1 extends Component {
   render() {
     return (
-      <div className="container-fluide d-flex flex-column">
-        <AuthContext.Provider value={this.state}>
-          <Layout />
-        </AuthContext.Provider>
+      <div className="card w-50">
+        <div className="card-header">User</div>
+        <div className="card-body">
+          <div className="card-title">{this.props.user.name}</div>
+          <div className="card-text">{this.props.user.city}</div>
+        </div>
       </div>
     );
   }
 }
 
+const WithLoader = (WrappedComposant, dataName) => {
+  return class extends Component {
+    render() {
+      return (
+        <>
+          {this.props[dataName] ? (
+            <WrappedComposant {...this.props} />
+          ) : (
+            <h1>Chargement en cours ...</h1>
+          )}
+        </>
+      );
+    }
+  };
+};
+
+const Composant1WithLoader = WithLoader(Composant1, "user");
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+
+    setTimeout(() => {
+      this.setState({
+        user: { name: "Nom", city: "Ville" },
+      });
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <div className="container-fluide d-flex flex-column justify-content-center align-items-center p-5">
+        <Composant1WithLoader user={this.state.user} />
+      </div>
+    );
+  }
+}
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
