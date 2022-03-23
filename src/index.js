a1a1ee634724627600b6b644972e56a9284ad21a
Pauhoa <hoarau.paul3@gmail.com>
2022-03-23 09:40:55 +0400

    formulaire avec formik

--- src/index.js
+++ src/index.js
@@ -1,42 +1,63 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { Formik } from 'formik';
 
-class Form extends Component {
-  value = {}
+class App extends React.Component {
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.submit(this.value); 
+  submit = values => {
+    console.log(values);
   }
-
-  handleChange = (e) => {
-    this.value[e.target.name] = e.target.value;
-  }
-
-  render() {
-    return <>{this.props.render({ handleSubmit: this.handleSubmit, handleChange: this.handleChange })}</>;
-  }
-}
-
-class App extends Component {
-  submit = (value) => {
-    console.log(value);
-  };
 
   render() {
     return (
-      <Form
-        submit={this.submit}
-        render={({ handleSubmit, handleChange}) => (
-          <form onSubmit={handleSubmit}>
-            <input name='name' onChange={handleChange} type="text" />
-            <input name='email' onChange={handleChange} type="text" />
-            <button>submit</button>
-          </form>
-        )}
-      />
-    );
+      <div className="container-fluid p-5 bg-light
+      d-flex flex-column justify-content-center align-items-center">
+        <Formik
+          onSubmit={this.submit}
+          initialValues={{name: '', email: '', password: ''}}
+        >
+          {({values,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+          isSubmitting}) => (
+            <form onSubmit={ handleSubmit } className="bg-white border p-5 d-flex flex-column">
+              <div className="form-group">
+                <label>Nom</label>
+                <input type="text"
+                      name="name"
+                      className="form-control"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.name} />
+              </div>
+              <div className="form-group">
+                <label>Adresse email</label>
+                <input type="email"
+                      name="email"
+                      className="form-control"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email} />
+              </div>
+              <div className="form-group">
+                <label>Mot de passe</label>
+                <input type="password"
+                      name="password"
+                      className="form-control"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password} />
+              </div>
+              <button type="submit" className="btn btn-primary" disabled={isSubmitting}>
+                Envoyer
+              </button>
+            </form>
+          )}
+        </Formik>
+      </div>
+    )
   }
 }
 
