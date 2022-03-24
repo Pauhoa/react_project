303b4610aa8b36817d82f7a1d85295f3e4ba6dd0
Pauhoa <hoarau.paul3@gmail.com>
2022-03-24 09:35:13 +0400

    utilisation de FieldArray

--- src/index.js
+++ src/index.js
@@ -1,74 +1,92 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Formik, Field, ErrorMessage} from 'formik';
-import * as Yup from 'yup';
-
+import { Formik, Field, ErrorMessage, FieldArray } from "formik";
+import * as Yup from "yup";
 
 const CustomInput = ({ field, form, ...props }) => {
   return (
     <div className="form-group">
-      <label>{field.name}</label>
+      <label>{ props.displayname ? props.displayname : field.name}</label>
       <input {...field} type="text" {...props} className="form-control" />
     </div>
-  )
-}
+  );
+};
 
 const CustomError = (props) => {
-  return (
-    <div className="alert alert-danger">{props.children}</div>
-  )
-}
+  return <div className="alert alert-danger">{props.children}</div>;
+};
 
 class App extends Component {
-
   userSchema = Yup.object().shape({
-    name: Yup.string().min(3, 'Nom trop court').max(7, 'Nom trop long').required('Nom obligatoire'),
-    email: Yup.string().email('Email non valide').required('Email obligatoire'),
-    password: Yup.string().min(5, 'Mot de passe trop court')
-  })
+    name: Yup.string()
+      .min(3, "Nom trop court")
+      .max(7, "Nom trop long")
+      .required("Nom obligatoire"),
+    email: Yup.string().email("Email non valide").required("Email obligatoire"),
+    password: Yup.string().min(5, "Mot de passe trop court"),
+    items: Yup.array().of(Yup.object().shape({
+      name: Yup.string().required('Nom obligatoire'),
+      quantity: Yup.number().typeError('Doit être un nombre').min(5, 'Nombre trop petit')
+    }))
+  });
 
   submit = (values, actions) => {
     // console.log(actions);
     // console.log(values);
     actions.setSubmitting(false);
-  }
-  
-
+  };
 
   render() {
     return (
       <div className="container-fluid p-5 bg-light d-flex flex-column justify-content-center align-items-center">
         <Formik
-        onSubmit={ this.submit }
-        initialValues={ { name: '', email: '', password: '' } }
-        validationSchema={this.userSchema}
+          onSubmit={this.submit}
+          initialValues={{ name: "", email: "", password: "", items: [] }}
+          validationSchema={this.userSchema}
         >
-          { ({
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            values,
-            isSubmitting,
-            errors,
-            touched
-          }) => (
-            <form onSubmit={ handleSubmit } className="bg-white border p-5 d-flex flex-column">
-              <Field name="name" component={ CustomInput } />
-              <ErrorMessage name="name" component={ CustomError }/>
-              
-              <Field name="email" type="email" component={ CustomInput } />
-              <ErrorMessage name="email" component={ CustomError }/>
+          {({ handleSubmit, isSubmitting, values }) => (
+            <form
+              onSubmit={handleSubmit}
+              className="bg-white border p-5 d-flex flex-column"
+            >
+              <Field name="name" component={CustomInput} />
+              <ErrorMessage name="name" component={CustomError} />
 
-              <Field name="password"  type="password"  component={ CustomInput } />
-              <ErrorMessage name="password" component={ CustomError }/>
+              <Field name="email" type="email" component={CustomInput} />
+              <ErrorMessage name="email" component={CustomError} />
 
-              <button type="submit" className="btn btn-primary" disabled={ isSubmitting } >Submit</button>
+              <Field name="password" type="password" component={CustomInput} />
+              <ErrorMessage name="password" component={CustomError} />
+              <FieldArray name="items">
+                {(arrayHelpers) => (
+                  <div>
+                    {values.items && values.items.length
+                      ? values.items.map((item, index) => (
+                          <div key={ index }>
+                            <div>Item : {index}</div>
+                            <hr className="w-100" />
+                            <Field name={`items.${index}.name`} displayname="name" component={CustomInput} />
+                            <ErrorMessage name={`items.${index}.name`} component={CustomError} />
+                            <Field name={`items.${index}.quantity`} displayname="quantity" component={CustomInput} />
+                            <ErrorMessage name={`items.${index}.quantity`} component={CustomError} />
+                            <button type="button" className="w-100 btn btn-small btn-danger" onClick={ () => arrayHelpers.remove(index)}>delete</button>
+                          </div>
+                        ))
+                      : null}
+                      <button type="button" className="mt-3 w-100 btn btn-small btn-success" onClick={ () => arrayHelpers.push({
+                        name: '',
+                        quantity: 0
+                      }) }>Add item</button>
+                  </div>
+                )}
+              </FieldArray>
+              <button type="submit" className="mt-3 btn btn-primary" disabled={isSubmitting}>Submit</button>
             </form>
           )}
         </Formik>
       </div>
-    )
+    );
   }
 }
 
